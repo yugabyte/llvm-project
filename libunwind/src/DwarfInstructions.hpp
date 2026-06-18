05bebec6d0725098b82c09e4ffdf5d0628c2f9cd
Eric Sheng <esheng@yugabyte.com>
2026-06-18 10:25:04 -0700

    [22.1.0] Fix unwind from signal handler
    Change taken from: https://github.com/ClickHouse/libunwind/commit/ba533a7246a2686b0552061809612f503804d26b
    
    In case of this is frame of signal handler, the IP should be incremented, because the IP saved in the signal handler points to first non-executed instruction, while FDE/CIE expects IP to be after the first non-executed instruction.
    
    This can cause SIGSEGV, since our stack tracing code involves libunwind via signal handlers.

--- libunwind/src/DwarfInstructions.hpp
+++ libunwind/src/DwarfInstructions.hpp
@@ -411,7 +411,13 @@ int DwarfInstructions<A, R>::stepWithDwarf(
 
       // Return address is address after call site instruction, so setting IP to
       // that does simulates a return.
-      newRegisters.setIP(returnAddress);
+      //
+      // In case of this is frame of signal handler, the IP should be
+      // incremented, because the IP saved in the signal handler points to
+      // first non-executed instruction, while FDE/CIE expects IP to be after
+      // the first non-executed instruction.
+      // Also see: https://github.com/ClickHouse/libunwind/commit/ba533a7246a2686b0552061809612f503804d26b
+      newRegisters.setIP(returnAddress + cieInfo.isSignalFrame);
 
       // Simulate the step by replacing the register set with the new ones.
       registers = newRegisters;
